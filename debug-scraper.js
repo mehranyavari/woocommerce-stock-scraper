668bd32248b8fa9226b3fe0b83a9c966648f1828
mehranyavari <81741646+mehranyavari@users.noreply.github.com>
2026-08-09 13:21:02 +0330

    Update debug-scraper.js

--- debug-scraper.js
+++ debug-scraper.js
@@ -6,7 +6,7 @@ let useProxy = true;
 // ==========================================
 // 🔗 لینک تست
 // ==========================================
-const TEST_URL = "https://www.tenisshop.com/en/nikecourt-lite-4-erkek-tenis-ayakkabisi-3337";
+const TEST_URL = "https://www.raketspor.com.tr/nikecourt-fn0530-001-lite-4-toprak-kort-erkek-tenis-ayakkabisi-siyah-14403";
 
 // ==========================================
 // تنظیمات
